97ed0402031b2fe77055d400401921f9783a6ea5
Jin Lou <jinlou@bu.edu>
2021-06-16 12:19:13 -0400

    Post Data to mailchimp server.

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require('https');
 
 const app = express();
 const port = 3000;
+
+const apiKey = "0f391543d53c2883bf69c5a918c713b1-us6";
+const audienceID = "526ba95e64";
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
@@ -13,6 +17,40 @@ app.get('/', function(req, res){
 })
 
 app.post('/', function(req,res){
+  const firstName = req.body.fname;
+  const lastName = req.body.lname;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+const url = "https://us6.api.mailchimp.com/3.0/lists/" + audienceID;
+
+const options = {
+  method: "POST",
+  auth: "jinlou98:"+apiKey
+}
+
+const request = https.request(url, options, function(response){
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    })
+  })
+
+request.write(jsonData);
+request.end();
 
 })
 
