03405f1ce15aff7e44a4026d7503eba4337dc15e
Jin Lou <jinlou@bu.edu>
2021-06-16 12:36:01 -0400

    Add success and failure page.

--- app.js
+++ app.js
@@ -36,22 +36,33 @@ app.post('/', function(req,res){
 
   const jsonData = JSON.stringify(data);
 
-const url = "https://us6.api.mailchimp.com/3.0/lists/" + audienceID;
+  const url = "https://us6.api.mailchimp.com/3.0/lists/" + audienceID;
 
-const options = {
-  method: "POST",
-  auth: "jinlou98:"+apiKey
-}
+  const options = {
+    method: "POST",
+    auth: "jinlou98:"+apiKey
+  }
 
-const request = https.request(url, options, function(response){
-    response.on("data", function(data){
-      console.log(JSON.parse(data));
+  const request = https.request(url, options, function(response){
+
+      if (response.statusCode === 200) {
+        res.sendFile(__dirname + '/success.html');
+      } else {
+        res.sendFile(__dirname + '/failure.html');
+      }
+
+      response.on("data", function(data){
+        console.log(JSON.parse(data));
+      })
     })
-  })
 
-request.write(jsonData);
-request.end();
+  request.write(jsonData);
+  request.end();
 
+})
+
+app.post('/failure.html', function(req,res){
+  res.redirect('/');
 })
 
 app.listen(port, function(){
